4b8475520750d5f60bfde567f72e1317f30f447c
zarab <musfiqzarab@iut-dhaka.edu>
2018-09-25 00:27:16 +0600

    % counters

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import "bootstrap/dist/css/bootstrap.css";
-import Counter from "./components/counter"; //Counter is default export .. so no need to use curly braces around it
+import Counters from "./components/counters"; //Counter is default export .. so no need to use curly braces around it
 
-ReactDOM.render(<Counter />, document.getElementById("root"));
+ReactDOM.render(<Counters />, document.getElementById("root"));
 registerServiceWorker();
