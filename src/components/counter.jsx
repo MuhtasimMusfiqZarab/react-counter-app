9005e198d47da3c5e67258283ca17009d8dc427a
zarab <musfiqzarab@iut-dhaka.edu>
2018-11-17 14:23:25 +0600

    final 1.0.0

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,19 +1,6 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  /* state = {
-    value: this.props.counter.value
-  };
-
-  /*styles = {
-    fontSize: 30,
-    fontWeight: "bold"
-  };*/
-
-  /* handleIncreament = () => {
-    console.log("Increament Clicked", this);
-    this.setState({ value: this.state.value + 1 });
-  };*/
   render() {
     // console.log("props", this.props);
     return (
